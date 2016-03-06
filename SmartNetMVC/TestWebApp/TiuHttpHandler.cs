bafa7a92a0f2b9c78a3cf24d51f68cff5c7a8be2
webxiaohua <webxiaohua@gmail.com>
2016-03-06 23:00:57 +0800

    啦啦啦

---
+++ SmartNetMVC/TestWebApp/TiuHttpHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp
+{
+    public class TiuHttpHandler : IHttpHandler
+    {
+        /// <summary>
+        /// 这http handle的实例是否能被重用来处理多个同类型的http请求
+        /// </summary>
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var currentResponse = context.Response;
+            var outPutStr = string.Format("Hello Tiu");
+            currentResponse.Write(outPutStr);
+        }
+    }
+}
