863f8f4c5dad75c2650abfe77b3c2c63ec928b27
sxh沈新华 <shen_xh@ctrip.com>
2016-03-07 21:11:23 +0800

    asdfaf

---
+++ SmartNetMVC/TestWebApp/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp
+{
+    public class HomeController
+    {
+        public string Index() {
+            return "Hello World";
+        }
+    }
+}
